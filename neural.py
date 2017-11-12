e64d10afe00818b4eb35c247d3d6fc0d7e9c04a5
Jadin Casey <jadin.c77@gmail.com>
2017-11-11 19:15:06 -0700

    fixed tab error

--- neural.py
+++ neural.py
@@ -9,14 +9,14 @@ num_hidden_nodes = 10         #per-layer
 
 #load a CSV file
 def load_csv(filename):
-	dataset = list()
-	with open(filename, 'r') as file:
-		csv_reader = reader(file, delimiter='\t')
-		for row in csv_reader:
-			if not row:
-				continue
-			dataset.append(row)
-	return dataset
+    dataset = list()
+    with open(filename, 'r') as file:
+        csv_reader = reader(file, delimiter='\t')
+        for row in csv_reader:
+            if not row:
+                continue
+            dataset.append(row)
+    return dataset
     
 #change data string to number
 def str_to_float(dataset):
@@ -35,22 +35,39 @@ def normalize_data(dataset):
         for i in range(len(row)):
             row[i] = (float(row[i]) - datamin[i]) / (datamax[i] - datamin[i])
             
+    
+def evaluate_network(network):
+    return
+    
+    
+def forward_propagate(network, dataset):
+    sum_error = 0
+    for i in range(0, data_set_size):
+        test_index = random.randint(0, data_set_size)
+        data = np.array(dataset[test_index])
+        input_to_hidden = np.cross(data, network[0])
+        input_to_hidden = np.tanh(input_to_hidden)
+        hidden_to_hidden = np.cross(input_to_hidden, network[1])
+        hidden_to_hidden = np.tanh(hidden_to_hidden)
+        hidden_to_out = np.sum(np.cross(hidden_to_hidden, network[2]))
+    return sum_error
+
 def create_network(list_i_h_edges, list_h_h_edges, list_h_o_edges):
     num_nodes = num_input_nodes + num_hidden_nodes * num_hidden_layers + num_output_nodes
     network = list()
-    for i in range(num_input_nodes):
-        for j in range(num_input_nodes+1, num_input_nodes + num_hidden_nodes):
+    for x in range (len(network)):
+        network[x] = []
+    for i in range(num_input_nodes-1):
+        for j in range(num_input_nodes, num_input_nodes + num_hidden_nodes-1):
             network[i][j] = list_i_h_edges[i]
-    stop_point = num_input_nodes + num_hidden_nodes + 1
-    for i in range(num_input_nodes+1, num_input_nodes + num_hidden_nodes):
-        for j in range(num_input_nodes + num_hidden_nodes + 1, num_input_nodes + num_hidden_nodes + num_hidden_nodes):
+    for i in range(num_input_nodes, num_input_nodes + num_hidden_nodes-1):
+        for j in range(num_input_nodes + num_hidden_nodes, num_input_nodes + num_hidden_nodes + num_hidden_nodes-1):
             network[i][j] = list_h_h_edges
     for i in range(num_input_nodes + num_hidden_nodes + 1, num_input_nodes + num_hidden_nodes + num_hidden_nodes):
         for j in range(num_input_nodes + num_hidden_nodes + num_hidden_nodes + 1, num_input_nodes + num_hidden_nodes + num_hidden_nodes + num_output_nodes):
             network[i][j] = list_h_o_edges
+    network = np.array(network)
     return network
-    
-def evaluate_network(network):
     
     
 #RUN
